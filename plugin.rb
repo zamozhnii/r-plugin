04bc4d7ebc93a52d797b9c928c5aa4a5f2dc2cab
ihor <zamozhnii@gmail.com>
2024-12-02 15:09:34 +0100

    fix

--- plugin.rb
+++ plugin.rb
@@ -1,16 +1,19 @@
 # name: r_plugin
 # about: Redirects all 404 errors to the home page
 # version: 0.1
-# authors: Your Name
+# authors: IDW
 # url: https://github.com/zamozhnii/r_plugin.git
 
-after_initialize do
-  # Перехватываем ошибки 404 и перенаправляем на главную страницу
-  Discourse::Application.routes.append do
-    match "/404", to: redirect('/', status: 301), via: :all
-  end
+enabled_site_setting :enable_r_plugin
 
-  rescue_from ActionController::RoutingError do |_exception|
-    redirect_to("/", status: 301)
+after_initialize do
+  if SiteSetting.enable_r_plugin
+    Discourse::Application.routes.append do
+      match "/404", to: redirect('/', status: 301), via: :all
+    end
+
+    rescue_from ActionController::RoutingError do |_exception|
+      redirect_to("/", status: 301)
+    end
   end
 end
