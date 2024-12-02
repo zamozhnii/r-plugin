c2bcf0c1d4968fcc38507699bd0c22028bcf1da7
ihor <zamozhnii@gmail.com>
2024-12-02 17:12:09 +0100

    fix

--- plugin.rb
+++ plugin.rb
@@ -6,26 +6,14 @@
 
 enabled_site_setting :enable_r_plugin
 
-def configure_routes
-  Discourse::Application.routes.prepend do
-    if SiteSetting.enable_r_plugin
-      match "/404", to: redirect('/', status: 301), via: :all
-      rescue_from ActionController::RoutingError do |_exception|
-        redirect_to("/", status: 301)
-      end
-    else
-      # Ничего не делаем, если настройка отключена
-      # Или можно удалить существующие маршруты, если они были добавлены ранее
-    end
-  end
-end
-
 after_initialize do
-  configure_routes
+  if SiteSetting.enable_r_plugin
+    Discourse::Application.routes.append do
+      match "/404", to: redirect('/', status: 301), via: :all
+    end
 
-  DiscourseEvent.on(:site_setting_updated) do |name, old_val, new_val|
-    if name == "enable_r_plugin"
-      Rails.application.reload_routes! if old_val != new_val
+    rescue_from ActionController::RoutingError do |_exception|
+      redirect_to("/", status: 301)
     end
   end
 end
